d5f2ed11d7f0b745d2b17854900149201075c3e3
dkckhaudhary033 <d.k.chaudhary033@gmail.com>
2023-08-17 21:19:49 +0530

    web-project-upload-17aug23

---
+++ JS_Basic/index.js
@@ -0,0 +1,19 @@
+console.log(`Hello Duniya! 
+me jaha par hoon.`);
+
+let rectangle = {
+    length  : 1,
+    breadth : 2,
+
+    draw: function(){
+        console.log("drawing a picture");
+    }
+
+}
+
+let person = {
+    fName : 'Deepak',
+    lName : 'Chaudhary',
+};
+    
+    console.log(peson);
